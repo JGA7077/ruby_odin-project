44c54455e71f1d27166949827e8bc1dad5dcb8e0
João Gabriel <joaogabriel-al@hotmail.com>
2025-08-18 19:34:32 -0300

    strings final

--- 01.b-strings.rb
+++ 01.b-strings.rb
@@ -28,3 +28,35 @@ puts "hello"[0]
 puts "hello"[0..3]
 puts "hello"[0, 3]
 puts "hello"[-1]
+
+puts "===================================="
+puts "Common strings methods"
+puts "===================================="
+"hello".capitalize # "Hello"
+
+"hello".include?("lo") # true
+"hello".include?("z") # false
+
+"hello".upcase # "HELLO"
+
+"HELLO".downcase # "hello"
+
+"hello".empty?  # false
+
+"hello".length  # 5
+
+"hello".reverse  # "olleh"
+
+"hello world".split  # ["hello", "world"]
+"hello".split("")    # ["h", "e", "l", "l", "o"]
+
+" hello, world   ".strip  # "hello, world"
+
+puts "===================================="
+puts "Converting other objects to strings"
+puts "===================================="
+5.to_s        # "5"
+
+nil.to_s      # ""
+
+:symbol.to_s  # "symbol"
