34876f756436ec70a122c585ba1ab493cdea54a7
João Gabriel <joaogabriel-al@hotmail.com>
2025-08-18 20:56:36 -0300

    conditions parte 2

---
+++ 02.b-conditional-logic.rb
@@ -0,0 +1,32 @@
+# Boolean logic
+
+puts "===================================="
+puts "Validando valores e tipos"
+
+five_integer = 5
+five_float = 5.0
+
+puts five_integer.eql?(five_float) #=> será considerado 'false' pois o primeiro é inteiro e o segundo float
+puts five_integer.eql?(5)   #=> true
+
+puts "===================================="
+puts "Equal?"
+
+# O equal? valida se os valores estão ocupando o mesmo espaço em memória, no caso dos inteiros, o computador consegue fazer com que ocupem o mesmo espaço na memória se forem do mesmo valor, então o seguinte código gera 'true'
+puts five_integer.equal?(5)
+
+# Já no caso das strings, não é possível fazer este gerenciamento, então como vimos antes, duas strings irão ocupar endereços diferentes na memória, mesmo se forem de valores iguais:
+
+str = 'teste'
+puts str.equal?('teste')
+
+puts "===================================="
+puts "Spaceship Operator"
+
+# -1 if the value on the left is less than the value on the right;
+# 0 if the value on the left is equal to the value on the right; and
+# 1 if the value on the left is greater than the value on the right.
+
+5 <=> 10    #=> -1
+10 <=> 10   #=> 0
+10 <=> 5    #=> 1
