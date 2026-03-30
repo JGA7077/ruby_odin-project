eae8ecba75580bfa66dc02c2093b808114575eb0
João Gabriel <joaogabriel-al@hotmail.com>
2026-03-30 14:59:50 -0300

    putting puts on spaceship operator results

--- 02.b-conditional-logic.rb
+++ 02.b-conditional-logic.rb
@@ -27,6 +27,8 @@ puts "Spaceship Operator"
 # 0 if the value on the left is equal to the value on the right; and
 # 1 if the value on the left is greater than the value on the right.
 
-5 <=> 10    #=> -1
-10 <=> 10   #=> 0
-10 <=> 5    #=> 1
+result1 = 5 <=> 10    #=> -1
+result2 = 10 <=> 10   #=> 0
+result3 = 10 <=> 5    #=> 1
+
+puts "1: #{result1}, 2: #{result2}, 3: #{result3}"
