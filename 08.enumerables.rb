6da7c52c7c85d06b401c7b341a960d4d3137bf25
João Gabriel <joaogabriel-al@hotmail.com>
2026-04-03 08:52:36 -0300

    some enumerables method

--- 08.enumerables.rb
+++ 08.enumerables.rb
@@ -18,3 +18,42 @@ friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
 invited_list = friends.select { |friend| friend != 'Brian' }
 
 puts "invited_list #{invited_list}"
+
+puts "===================================="
+puts "The each method"
+
+friends.each { |friend| puts "Hello, #{friend}" }
+
+my_array = [1, 2]
+
+my_array.each do |num|
+  num *= 2
+  puts "The new number is #{num}."
+end
+
+puts "Each with hashes ==================="
+my_hash = { "one" => 1, "two" => 2 }
+
+puts "\nas key and value:"
+my_hash.each { |key, value| puts "#{key} is #{value}" }
+
+puts "\nas pair:"
+my_hash.each { |pair| puts "the pair is #{pair}" }
+
+puts "===================================="
+puts "The each_with_index method"
+
+fruits = ["apple", "banana", "strawberry", "pineapple"]
+
+fruits.each_with_index { |fruit, index| puts fruit if index.even? }
+
+puts "===================================="
+puts "The map method"
+
+my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
+
+my_order_mapped = my_order.map { |item| item.gsub('medium', 'extra large') }
+puts "my_order_mapped #{my_order_mapped}"
+
+puts "===================================="
+puts "The select method"
