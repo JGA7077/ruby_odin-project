860d9fade69931f16885e1d1bbcfcd336a4922ee
João Gabriel <joaogabriel-al@hotmail.com>
2026-04-06 13:20:53 -0300

    reduce with pre value

--- 08.enumerables.rb
+++ 08.enumerables.rb
@@ -69,3 +69,8 @@ my_numbers = [5, 6, 7, 8]
 
 my_numbers.reduce { |sum, number| sum + number }
 #=> 26
+
+my_numbers = [5, 6, 7, 8]
+
+my_numbers.reduce(1000) { |sum, number| sum + number }
+#=> 1026
