cbd414e8099daa9d9fd19ec67cd99059424eedef
João Gabriel <joaogabriel-al@hotmail.com>
2026-04-07 16:34:46 -0300

    enumerables

--- 08.enumerables.rb
+++ 08.enumerables.rb
@@ -70,7 +70,14 @@ my_numbers = [5, 6, 7, 8]
 my_numbers.reduce { |sum, number| sum + number }
 #=> 26
 
-my_numbers = [5, 6, 7, 8]
-
 my_numbers.reduce(1000) { |sum, number| sum + number }
 #=> 1026
+
+votes = ["Bob's Dirty Burger Shack", "St. Mark's Bistro", "Bob's Dirty Burger Shack"]
+
+result = votes.reduce(Hash.new(0)) do |result, vote|
+  result[vote] += 1
+  result
+end
+
+puts "result #{result}"
